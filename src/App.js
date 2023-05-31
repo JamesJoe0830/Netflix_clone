4bc5b41943be4d36be8d52b8892839826d8dc9c0
joejames0830 <kyeun950830@gmail.com>
2023-05-31 10:13:22 +0900

    :

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components.Navjs';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "6d5dddd3f5d9d13c74e37b8e58006df8",
+    language: "ko-KR",
+  },
+});
+
+export default instance;
